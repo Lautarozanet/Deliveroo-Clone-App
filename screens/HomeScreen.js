71fa68d81b0a2fb806363ec4e858095807092b9e
Lautaro.z <lautaro.zanet@outlook.com>
2023-01-13 19:50:44 -0300

    actualizando

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, SafeAreaView,Image, TextInput } from 'react-native'
+import { View, Text, SafeAreaView,Image, TextInput, ScrollView } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { useNavigation } from '@react-navigation/native';
 import {
@@ -7,6 +7,7 @@ import {
     MagnifyingGlassCircleIcon,
     AdjustmentsVerticalIcon,
 } from "react-native-heroicons/outline";
+import Categories from '../components/Categories';
 
 const HomeScreen = () => {
     const navigation=useNavigation();
@@ -18,13 +19,9 @@ const HomeScreen = () => {
     },[]);
 
   return (
-    <SafeAreaView className="bg-white pt-5">
-         <View>
-            <Text></Text>
-            </View>
-      <Text className="text-red-500">
+    <SafeAreaView className="bg-white pt-8">
         {/* header */}
-        <View className="flex-row pb-3 items-center mx-4 space-x-2">
+        <View className="flex-row pb-3 items-center mx-4 space-x-2 px-4">
             <Image
             source={{
                 uri:"https://links.papareact.com/wru",
@@ -45,8 +42,8 @@ const HomeScreen = () => {
         </View>
         
             {/* search */}
-            <View>
-                <View className="flex-row space-x-2 flex-1 bg-gray-200 p-3">
+            <View className="flex-row items-center space-x-2 pb-2 mx-4 px-4">
+                <View className="flex-row flex-1 space-x-2 bg-gray-200 p-3">
                     < MagnifyingGlassCircleIcon color="gray" size={20}/>
                     <TextInput placeholder='Restaurants and cuisines'
                     keyboardType='default'/>
@@ -54,7 +51,16 @@ const HomeScreen = () => {
 
                 <AdjustmentsVerticalIcon color="#00CCBB"/>
             </View>
-      </Text>
+            {/* Body */}
+            <ScrollView className="bg-gray-100"
+            contentContainerStyle={{
+                paddingBottom:100,
+            }}>
+                {/* Categories */}
+                <Categories></Categories>
+
+                {/* Features Rows */}
+            </ScrollView>
     </SafeAreaView>
   );
 };
